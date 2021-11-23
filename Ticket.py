bad7faeb4ca845aab83b2f1e9dee8ad40ef947f7
cdautori <cdautori@uncc.edu>
2021-11-23 17:54:40 -0500

    Moved code from Ticket.py to TicketViewer.py
    Had wrong file open in IDE - Ticket.py is meant to be an object to hold individual Ticket information, TicketViewer.py is the main program driver.

--- Ticket.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# Default parameters
-url = "https://zccstudents9591.zendesk.com/api/v2/groups.json"
-username = ""
-password = ""
-
---
+++ TicketViewer.py
@@ -0,0 +1,7 @@
+import requests
+
+# Default parameters
+url = "https://zccstudents9591.zendesk.com/api/v2/groups.json"
+username = ""
+password = ""
+
