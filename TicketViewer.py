d98f5a44dba50b688fd6598af8a183ffec4adb08
cdautori <cdautori@uncc.edu>
2021-11-23 18:29:59 -0500

    Replaced password with API token. Wrote "create_config" function to generate a config file if none is present. Wrote "main" method to work as the program driver.

--- TicketViewer.py
+++ TicketViewer.py
@@ -1,7 +1,32 @@
 import requests
 import configparser
+from os.path import exists
 
-# Default parameters
+# Global parameters
 url = "https://zccstudents9591.zendesk.com/api/v2/groups.json"
 username = ""
-password = ""
+apiToken = ""
+
+
+# Create config file
+def create_config():
+    config = configparser.ConfigParser()
+    config["USERINFORMATION"] = {
+        "Username": "",
+        "API Token": ""
+    }
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
+
+
+# Main function of program
+def main():
+    # Check if ini file exists. If not, create ini file
+    if not exists("config.ini"):
+        create_config()
+        print("Config file not found! Config file has been created at program root. Please enter your information"
+              "into the file config.ini and relaunch the program.")
+
+
+# Runs main function
+main()
